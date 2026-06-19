e12e0f03b6837a9b8daf589e9b5fdbee642ff29e
Hashem <trainee.hashem.znati@sigmalabs.co.uk>
2026-06-19 15:08:03 +0100

    removed functions related to misunderstanding of level 1 brief

--- Assessment-Fundamentals-Week-1-main/Level-1/task_one.py
+++ Assessment-Fundamentals-Week-1-main/Level-1/task_one.py
@@ -7,34 +7,6 @@ The only variable you are allowed to use in the global scope is the basket below
 basket = []
 
 
-def add_product() -> str:
-    """Returns a string of the product the user inputs"""
-    return input("What product would you like to add? ")
-
-
-def add_price(product: str) -> float:
-    product = add_product()
-    return input(f"What is the price of {product}? ")
-
-
-def check_valid_price(price: float) -> bool:
-    product = add_product()
-    price = None
-
-    while not isinstance(price, float) or isinstance(price, int):
-        print("You must enter a valid number for the price!")
-        price = add_price(product)
-    return True
-
-
-def create_item(product: str, price: float) -> dict:
-    item = {}
-    product = add_product()
-    price = add_price()
-    item[product] = price
-    return item
-
-
 def add_to_basket(item: dict) -> list:
     basket.append(item)
     return basket
